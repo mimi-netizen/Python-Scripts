e93ebdc2a829d627904bcbc95636432ee51f63f3
Celyne <dianakydd2@gmail.com>
2024-11-17 16:01:04 +0300

    initial commit

---
+++ Web Scrapers/HTML Scraper/amazon_scraper.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import requests
+import csv
+import bs4
+
+
+
+if __name__ == "__main__":
+    with open('amazon_products_urls.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            url = row[0]
+            print(url)
